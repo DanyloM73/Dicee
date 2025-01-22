47ec4027df0f43c383ce1f78bd43f58c15f820ab
DanyloM73 <danyamalanin7@gmail.com>
2025-01-22 19:18:57 +0200

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by danylo on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
